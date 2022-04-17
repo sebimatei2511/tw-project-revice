ef30264981a8f3c2fad1031518ddb6673591bbd8
sebimatei2511 <79249972+sebimatei2511@users.noreply.github.com>
2022-04-17 23:35:43 +0300

    Add files via upload

--- Revice/scripts/signupModal.js
+++ Revice/scripts/signupModal.js
@@ -1,7 +1,7 @@
-   var modal = document.getElementById('id01');
+var modal = document.getElementById('id01');
 
-    window.onclick = function(event) {
-            if (event.target == modal) {
-        modal.style.display = "none";
-            }
-            }
+window.onclick = function(event) {
+    if (event.target == modal) {
+          modal.style.display = "none";
+    }
+}
